fc84f2b2c0c16f4b5f321dab32b31d120a3a552f
ayushdev351 <mrayushchauhan351@gmail.com>
2023-07-08 11:23:52 +0530

    Component Composition

--- 2/App.js
+++ 2/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// React element -> Using react.createElement
-const parent = React.createElement("div", {id : "parent"}, 
-    React.createElement("div", {id : "child"},[
-        React.createElement("h1", {key : 1}, "Helloo React h1"),
-        React.createElement("h2", {key : 2}, "Helloo React h2")
-    ]));
+// Component Composition
 
-// React Element -> Using JSX
-const jsxElement = <h1>Hi from JSX Element</h1>;
-
-// React Functional Component -> Returns react element created using JSX
-const FunctionalComponent = () => {
+const Header = () => {
     return(
         <div>
-            <h1>Hi from functional Component</h1>
-            {jsxElement}
+            <h1>Header</h1>
+            <HeaderElement/>
+            <HeaderElement/>
+            <HeaderElement/>
+        </div>
+    )
+}
+
+const HeaderElement = () => {
+    return(
+        <div>
+            <h3>Header Element</h3>
         </div>
     )
 }
@@ -24,4 +25,4 @@ const FunctionalComponent = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Here object is converted into actual DOM element
-root.render(<FunctionalComponent/>)
+root.render(<Header/>)
