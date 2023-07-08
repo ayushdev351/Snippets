43431018b4474179b8c524a92b3e60a3c0cb6055
ayushdev351 <mrayushchauhan351@gmail.com>
2023-07-08 11:08:14 +0530

    React Element using JSX

--- 2/App.js
+++ 2/App.js
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// createElement is a legacy API. 
-// Heading is an object.
-// An element is a lightweight description of a piece of the user interface. 
-
+// React element -> Using react.createElement
 const parent = React.createElement("div", {id : "parent"}, 
     React.createElement("div", {id : "child"},[
         React.createElement("h1", {key : 1}, "Helloo React h1"),
         React.createElement("h2", {key : 2}, "Helloo React h2")
     ]));
 
-{/* 
-<div id = "parent">
-    <div id = "child">
-        <h1>Hello React h1</h1>
-        <h2>Hello React h2</h2>
-    </div>
-</div>
-*/}
+// React Element -> Using JSX
+const jsxElement = <h1>Hi from JSX Element</h1>;
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Here heading object is converted into actual DOM element
-root.render(parent)
+root.render(jsxElement)
