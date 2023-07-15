05cca466c13e354f023f561f75c357bc18069bcd
ayushdev351 <mrayushchauhan351@gmail.com>
2023-07-15 12:50:32 +0530

    Added class based component

--- 3/App.js
+++ 3/App.js
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "/contact",
-                element: <Contact/>
+                element: <Contact data = "Props data"/>
             },
             {
                 path: "/help",
--- 3/components/Contact.js
+++ 3/components/Contact.js
@@ -1,9 +1,31 @@
-const Contact = () => {
-    return(
-        <div>
-            Contact
-        </div>
-    )
+import React from "react";
+
+class Contact extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count : 0,
+        }
+    }
+
+    render() {
+        const counter = () => {
+            this.setState({
+                count : this.state.count + 1,
+            })
+        }
+
+        return (
+            <div>
+                <h1>Contact</h1>
+                <h1>Props : {this.props.data}</h1>
+                <h1>State : {this.state.count}</h1>
+                <button onClick={counter}>Click</button>
+            </div>
+        )
+    }
 }
 
-export default Contact;
+export default Contact
