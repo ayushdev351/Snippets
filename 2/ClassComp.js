10b39b0995e13545078781b0ea1c1b6194483de0
ayushdev351 <mrayushchauhan351@gmail.com>
2023-07-16 19:26:09 +0530

    Added routes -> Behaviour of component on unmounting

--- 2/ClassComp.js
+++ 2/ClassComp.js
@@ -8,47 +8,44 @@ class Parent extends React.Component{
         super(props);
 
         this.state = {
-            count : 0,
+            name : "John",
         }
 
         console.log("Parent Constructor called");
     }
 
-    componentDidMount(){
+    async componentDidMount(){
+        
         console.log("Parent Component Mounted")
+
+        const data = await fetch("https://api.github.com/users/ayushdev351");
+        const json = await data.json();
+
+        this.setState({
+            name : json.name,
+        })
     }
 
-
-    // This is only called when state gets updated......otherwise it wont be called.
+    // This is only called when state gets updated -> It is not called on the initial render but only after 
+    // setState is called...... i.e when states are updated
+    // otherwise it wont be called.
     componentDidUpdate(){
-        console.log("Parent Component Updated")
+        console.log("Parent Component Updated");
     }
 
     render(){
-
-        const counter = () => {
-            this.setState({
-                count : this.state.count + 1,
-            })
-        }
 
         console.log("Parent Rendered")
 
         return(
             <div>
-                <h1>Class Component State : {this.state.count}</h1>
-                <button onClick={counter}>Change State Button</button>
-                <Child name = "1"/>
-                <Child name = "2"/>
+                <h1>Class Component State : {this.state.name}</h1>
+                <button onClick={this.handleClick}>Make API Call</button>
+                {/* <Child name = "1"/>
+                <Child name = "2"/> */}
             </div>
         )
     }
 }
 
 export default Parent
-
-/*
-Parent Constructor Called
-Parent Render Called
-
-*/ 
