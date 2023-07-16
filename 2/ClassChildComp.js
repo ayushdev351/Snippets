9127353135314b95c3a47b880b39e82dc7b48840
ayushdev351 <mrayushchauhan351@gmail.com>
2023-07-16 17:23:03 +0530

    Parent Child Component Did Update

--- 2/ClassChildComp.js
+++ 2/ClassChildComp.js
@@ -4,6 +4,10 @@ class Child extends React.Component{
 
     constructor(props){
         super(props);
+        
+        this.state = {
+            count : 1,
+        }
 
         console.log("Child " + this.props.name + " Constructor")
     }
@@ -12,13 +16,24 @@ class Child extends React.Component{
         console.log("Child "+  this.props.name  + " Component Mounted")
     }
 
+    componentDidUpdate(){
+        console.log("Child "+  this.props.name  + " Component Updated")
+    }
+
     render(){
+        
+        const counter = () => {
+            this.setState({
+                count : this.state.count + 1,
+            })
+        }
 
         console.log("Child " + this.props.name + " Rendered")
 
         return(
             <div>
-                <h1>Hello {this.props.name}</h1>
+                <h1>Hello {this.props.name} : {this.state.count}</h1>
+                <button onClick={counter}>Change Child State Button</button>
             </div>
         )
     }
--- 2/ClassComp.js
+++ 2/ClassComp.js
@@ -18,13 +18,26 @@ class Parent extends React.Component{
         console.log("Parent Component Mounted")
     }
 
+
+    // This is only called when state gets updated......otherwise it wont be called.
+    componentDidUpdate(){
+        console.log("Parent Component Updated")
+    }
+
     render(){
+
+        const counter = () => {
+            this.setState({
+                count : this.state.count + 1,
+            })
+        }
 
         console.log("Parent Rendered")
 
         return(
             <div>
-                <h1>Class Component</h1>
+                <h1>Class Component State : {this.state.count}</h1>
+                <button onClick={counter}>Change State Button</button>
                 <Child name = "1"/>
                 <Child name = "2"/>
             </div>
